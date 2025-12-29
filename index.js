7b320dd2ab32dbb64e004cadeb08010a02110126
Dean Howe <hideanhowe@gmail.com>
2025-12-29 17:28:28 +0000

    solution

--- index.js
+++ index.js
@@ -14,20 +14,53 @@
 
 function wiseSpeak(sentence) {
   console.log(sentence);
+
+  // remove punctuation
+  const punctuation = sentence.slice(-1);
+  let cleanSentence = sentence.slice(0, -1);
+
   // words array
   const words = ['have', 'must', 'are', 'will', 'can'];
-
   // split into an array
-  const sentArr = sentence.split(' ');
+  const sentArr = cleanSentence.split(' ');
   // find first occurence
   console.log(sentArr);
+
+  // use findIndex()
+  const pivotIndex = sentArr.findIndex((word) =>
+    words.includes(word.toLowerCase())
+  );
+  console.log(pivotIndex);
+
+  // Slice array using pivotIndex
+  const part1 = sentArr.slice(0, pivotIndex + 1);
+  const part2 = sentArr.slice(pivotIndex + 1);
+
+  // lowercase part1
+  const lowerPart1 = part1.map((x) => x.toLowerCase());
+
+  console.log(part2, lowerPart1);
+
+  // form return sentence
+  const returnSentence = `${part2.join(' ')}, ${lowerPart1.join(
+    ' '
+  )}${punctuation}`;
+  console.log(returnSentence);
+
+  // uppercase first letter
+  const firstLetter = returnSentence[0].toUpperCase();
+  console.log(firstLetter);
+
+  return `${firstLetter}${returnSentence.slice(1)}`;
+
   // loop through sentence arr and find a word match
-  for (const [word, i] of sentArr) {
-    if (words.includes(word)) {
-      // get the index
-      console.log(i);
-    }
-  }
+  // for (const [word, i] of sentArr) {
+  // this doesn't work as expected!
+  //     if (words.includes(word)) {
+  // get the index
+  //      console.log(i)
+  //     }
+  // }
 
   // move all words before & including to end of sentence
   // // preserve order
@@ -39,4 +72,5 @@ function wiseSpeak(sentence) {
   // Keep punctuation at end of sentence
 
   // make sure only single space between words
+  // return sentence;
 }
